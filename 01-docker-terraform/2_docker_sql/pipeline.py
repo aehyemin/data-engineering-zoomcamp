22f851d4062ff40d07834ca6bf163c13167aed72
aehyemin <hsy13467926@gmail.com>
2025-07-09 16:02:09 +0900

    1.1.1 DE Introduction to Docker

--- 01-docker-terraform/2_docker_sql/pipeline.py
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -1,11 +1,22 @@
 import sys
-
 import pandas as pd
+
+# some fancy stuff with pandas
 
 print(sys.argv)
 
 day = sys.argv[1]
 
-# some fancy stuff with pandas
+print(f"job finished for day = {day}")
 
-print(f'job finished successfully for day = {day}')
+# import sys
+
+# import pandas as pd
+
+# print(sys.argv)
+
+# day = sys.argv[1]
+
+# # some fancy stuff with pandas
+
+# print(f'job finished successfully for day = {day}')
